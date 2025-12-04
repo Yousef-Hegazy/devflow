bd326b62427c4e64e981f731ada52b79fac4075a
Yousef-Hegazy <yousefashraf14725@gmail.com>
2025-12-04 23:05:09 +0300

    Refactor Home component to display questions and add Ask a Question button

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,27 @@
-import { isAuthenticated } from "@/lib/helpers/server";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default async function Home() {
-  const isAuth = await isAuthenticated();
-
   return (
-    <div>
-      <p>Hello World</p>
-    </div>
+    <>
+      <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+
+        <Button
+          render={<Link href="/ask-question" />}
+          className="primary-gradient! text-light-900! min-h-[46px]! border-0! px-4! py-3!"
+        >
+          Ask a Question
+        </Button>
+      </section>
+      <section className="mt-11">LocalSearch</section>
+      HomeFilter
+      <div className="mt-10 flex w-full flex-col gap-6">
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+      </div>
+    </>
   );
 }
